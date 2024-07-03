4e38f37203679425f6ad6cc11382f9d3858a76d1
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-07-03 17:42:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ STKR = os.environ.get("STKR")
 StartTime = time.time()
 Mukesh = Client(
     "chat-gpt" ,
-    api_id = 26480985
+    api_id = '26480985'
     api_hash = '56c935fae1c5c86ba5a3af655f8caa9d'
     bot_token = '7167158774:AAGEdIjUMO2ro-GoHayd2BV_6o4rADuplH0'
 )
